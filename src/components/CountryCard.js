ad0c64c7ba4c63902f075473f923bdb3d99fc532
ahmedazad <ahmedazad750@gmail.com>
2022-08-31 14:11:13 +0300

    card redesign

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -4,6 +4,7 @@ import {
   CardHeader,
   CardBody,
   Typography,
+  Button,
 } from "@material-tailwind/react";
 
 import { Link } from "react-router-dom";
@@ -11,18 +12,21 @@ import { Link } from "react-router-dom";
 export default function CountryCard({ text, image, cca2 }) {
   return (
     // first put card inside of [Link] tag so that the whole card will work like a button
-    <Link className="" to={`/country/${cca2}`}>
-      {/* card from material tailwind */}
-      <Card className="w-full shadow-none transition-all hover:shadow-2xl bg-light-blue-50 hover:bg-light-blue-100">
-        <CardHeader floated={false} className=" ">
-          <img src={image} alt="profile-picture" className=" w-full h-full" />
-        </CardHeader>
-        <CardBody className="text-center">
-          <Typography variant="h4" color="blue-gray" className="mb-2">
-            {text}
-          </Typography>
-        </CardBody>
-      </Card>
-    </Link>
+    <Button variant="text" className=" p-0">
+      <Link className="" to={`/country/${cca2}`}>
+        {/* put card inside of Button to give it material design look */}
+        {/* card from material tailwind */}
+        <Card className="w-full shadow-none transition-all bg-transparent">
+          <CardHeader floated={false} className=" ">
+            <img src={image} alt="profile-picture" className=" w-full h-full" />
+          </CardHeader>
+          <CardBody className="text-center">
+            <Typography variant="h4" color="blue-gray" className="mb-2">
+              {text}
+            </Typography>
+          </CardBody>
+        </Card>
+      </Link>
+    </Button>
   );
 }
